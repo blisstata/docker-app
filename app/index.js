e07b2a90e91c054d24fbdb325f0ca0aa414647d8
Srividya Tata <srividya.tata@zalando.de>
2018-09-22 20:05:05 +0200

    adding new dockerfile

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log("hello World");
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.end('Hi everybody!');
+});
+server.listen(7777, function(){
+	console.log("entered server")
+});
